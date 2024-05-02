1470225a76157d80d930bc47863d18e3492c0c4c
Kalpesh Ghodekar <kalpeshghodekar2004@gmail.com>
2024-05-02 21:02:10 +0530

    bug fix

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -14,22 +14,20 @@ import { singleUpload } from "../middlewares/multer.js";
 
 const app = express.Router();
 
-//To Create New Product  - /api/v1/product/new
 app.post("/new", adminOnly, singleUpload, newProduct);
 
-//To get all Products with filters  - /api/v1/product/all
+
 app.get("/all", getAllProducts);
 
-//To get last 10 Products  - /api/v1/product/latest
 app.get("/latest", getlatestProducts);
 
-//To get all unique Categories  - /api/v1/product/categories
+
 app.get("/categories", getAllCategories);
 
-//To get all Products   - /api/v1/product/admin-products
+
 app.get("/admin-products", adminOnly, getAdminProducts);
 
-// To get, update, delete Product
+
 app
   .route("/:id")
   .get(getSingleProduct)
